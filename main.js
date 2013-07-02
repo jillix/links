e40f2cc0f8be4f664d09a965a81bf2887fd621f9
Ionica Bizau <bizauionica@yahoo.com>
2013-07-02 19:37:20 +0300

    Initial commit.

---
+++ main.js
@@ -0,0 +1,12 @@
+var Bind = require("github/jillix/bind");
+var Events = require("github/jillix/events");
+
+module.exports = function(config) {
+
+    var self = this;
+    Events.call(self, config);
+
+    self.updateLinks = function (a, b, c, d) {
+        debugger;
+    };
+};
