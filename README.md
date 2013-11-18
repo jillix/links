dfcef69519a6bd9fb9da61ec9fd0bdba54f32130
Ionică Bizău <bizauionica@yahoo.com>
2013-11-18 19:08:01 +0200

    Adding prefix option. Updating to the latest stable releases of Bind and Events. New version: v0.1.3

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Mono module that will show/remove HTML elements using the login module data.
 
 # Changelog
 
+### v0.1.3
+ - Use `config.options.prefix` to set the prefix of role class (default: `"role-"`);
+ - Update to Bind v0.2.1 and Events v.0.1.7
+ - Added the `processConfig` function
+ - Added comments to understand the code better
+
 ### v0.1.2
  - Don't remove the role from the role array if data is `undefined`
 
--- main.js
+++ main.js
@@ -3,35 +3,89 @@ var Events = require("github/jillix/events");
 
 module.exports = function(config) {
 
+    // get self (the module)
     var self = this;
-    Events.call(self, config);
 
-    for (var i in config.roles) {
-        $("." + config.roles[i]).hide();
+    // set the config
+    self.config = config;
+    processConfig(config);
+
+    // hide all elements on THE PAGE that has classes containg
+    // preffix + role_name
+    for (var i = 0; i < config.roles.length; ++i) {
+        $("." + getRoleWithPrefix.call(self, config.roles[i])).hide();
     }
 
-    var cache;
-
+    /*
+     *  updateLinks
+     *
+     *  This function is called via Events module using the listen -> handler
+     *  configuration from the descriptor file.
+     *
+     *  At least one function call must
+     *  contain the userInfo sent by the Login module ('userInfo' event)
+     *
+     * */
     self.updateLinks = function (userInfo) {
 
-        cache = cache || userInfo;
-        var userData = cache || {};
+        // create the ca cache
+        self.cache = self.cache || userInfo;
 
+        // get the user data
+        var userData = self.cache || {};
+
+        // get the user data role
         userData.role = userData.role || config.publicRole;
 
-        $("." + userData.role).show();
+        // show the elements that has classes containing the role name
+        $("." + getRoleWithPrefix.call(self, userData.role)).show();
+
+        // get the index of the current role
         var index = config.roles.indexOf(userData.role);
 
+        // if userinfo is not defined and index is not -1
         if (userInfo && index !== -1) {
+            // remove the role from roles
             config.roles.splice(index, 1);
         }
 
+        // remove all elements that contain the roles and not the current role
         for (var i = 0; i < config.roles.length; ++i) {
-            $("." + config.roles[i] + ":not('." + userData.role + "')").remove();
+            $(
+                "." +
+                getRoleWithPrefix.call(self, config.roles[i]) +
+                ":not('." +
+                getRoleWithPrefix.call(self, userData.role) +
+                "')"
+             ).remove();
         }
 
+        // emit 'updatedLinks' event
         self.emit("updatedLinks", userData);
     };
 
+    // call Events
+    Events.call(self, config);
+
+    // emit 'ready' event
     self.emit("ready");
 };
+
+/*
+ *  This returns the role with its prefix set in the
+ *  module configuration
+ *
+ * */
+function getRoleWithPrefix (role) {
+    return this.config.options.prefix + role;
+}
+
+/*
+ * Process config function
+ *
+ * */
+function processConfig (config) {
+    config.options = config.options || {};
+    config.options.prefix = config.options.prefix || "role-";
+    config.roles = config.roles || [];
+}
